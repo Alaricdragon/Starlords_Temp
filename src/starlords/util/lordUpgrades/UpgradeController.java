72cf560189174252d4ac3d6f0953a222876b239e
Alaricdragon <alaric.meservy@gmail.com>
2025-09-27 17:41:58 -0700

    notes because its been a while

--- src/starlords/util/lordUpgrades/UpgradeController.java
+++ src/starlords/util/lordUpgrades/UpgradeController.java
@@ -17,6 +17,31 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class UpgradeController {
+    /*ok... ok...
+    * so heres what I know:
+    * 1) I asked in the misc modding questions how to read a CSV file. I think that works, but it needs testing.
+    * 2) I need to spend some time considering how to organize the upgrade systems, and how to determine what upgrade I should be using for a given ship
+    *
+    * notes on upgrade CSV:
+    * 1) make it so the upgrade CSV can have multiple string Ids that match to a starlords ability to upgrade (for both the weight of preforming this upgrade, the AI weight of preforming a upgrade (unused until I fix the fucking AI), and for the cost of the upgrade)
+    *    please note that the string Ids would need to be placed within a starlord in the starlord.json (with a value between -1 and 1), like so:
+    *    "upgradeData":{
+    *       "upgradeType":{
+    *           "enabled": true,
+    *
+    *           "thingA": 0.75,
+    *           "thingB": -0.15,
+    *           "thingC": 0
+    *       }
+    *   }
+    *
+    * 2) make it so a given upgrade can have the 'default random' boolean. if true, the values of a given upgrade for each starlord would be randomized on starlord creation. otherwise, they would be zero.
+    *    -note: have this be changeable in the class as well. just to make things easyer
+    * 3) make it so there is a 'default enabled' boolean. if true, the upgrade can be used by everyone by default.
+    * 4) remember to make everything modifiable in the 'upgrade' script. the one matching to each and every upgrade type (by default, this will be officers, ships, S-mods, and so forth.)
+    * 5) the 'group' data allows a given upgrade to be added to a group. so you can enable and disable them in groups.
+    *    if something is part of a 'group'....
+    *    ... I dont like groups. I dont like them at all. they might be needed later, but for now let sleeping dogos lie.*/
     @Getter
     private static HashMap<String, UpgradeBase> upgrades = new HashMap<>();
     @Getter
